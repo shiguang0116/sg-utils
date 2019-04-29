69d6ea737d1b7d249583e0fccc63de2da9ed15ab
guang.shi <shiguang0116@163.com>
2019-04-29 15:15:37 +0800

    node服务文件优化

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
  * @description: node服务配置文件
  * @author: guang.shi <https://blog.csdn.net/guang_s> 
  * @date: 2019-04-10 10:02:24 
+ * 
+ * 启动服务:  node server
  */
 
 const http = require('http');
@@ -14,7 +16,7 @@ const port = 8080;          // 设置端口
 const openPage = '/demo/demo.html';             // 默认打开的页面
 const rootPath = path.resolve(__dirname, './'); // 文件的访问路径
 
-// 创建服务
+// 创造一个服务器实例（req:客户端的HTTP请求 | res:服务器端的HTTP回应）
 const server = http.createServer(function (req, res) {
     const url = req.url;            // 客户端输入的url，如：输入 localhost:8080/index.html，那么 url = '/index.html'
     const file = rootPath + url;    // 访问的文件路径
@@ -36,7 +38,7 @@ const server = http.createServer(function (req, res) {
     });
 });
 
-// 输入命令 node server 启动服务
+// 启动服务器监听指定端口
 server.listen(port, host, function(){
     console.log('\x1B[36m%s\x1B[39m', `服务器运行在: \n - http://${host}:${port} \n - http://${host}:${port}${openPage}`);
 });
